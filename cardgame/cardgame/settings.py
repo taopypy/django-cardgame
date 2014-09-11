8b23ac904d4457923669313f663d552a368f68bd
valdergallo <valdergallo@gmail.com>
2014-09-11 17:57:59 -0300

    add media root

--- cardgame/cardgame/settings.py
+++ cardgame/cardgame/settings.py
@@ -95,6 +95,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 REST_FRAMEWORK = {
